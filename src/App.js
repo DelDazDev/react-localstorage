af715bcd732a740ecfa623c54567db09ef9f0196
carlos del saz <delsaz9999@gmail.com>
2023-12-07 17:40:10 +0100

    Primer commit app Fazt tareas 7/12/23

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+import { useState } from "react";
 import './App.css';
+import TaskCreator from "./components/TaskCreator";
+
 
 function App() {
+
+  const [tasksItem, setTasksItem] = useState([
+    { name: "Mi primera tarea", done: false },
+    { name: "Mi segunda tarea", done: false },
+    { name: "Mi tercera tarea", done: false },
+  ]);
+
+  function createNewTask(taskName) {// Agregar las nuevas tareas escritas en el input a la lista ya hecha.
+    // Comprobamos que no haya una misma tarea.
+    if (!tasksItem.find(task => task.name === taskName)) {
+      setTasksItem([...tasksItem, { name: taskName, done: false }])
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TaskCreator
+        createNewTask={createNewTask}
+      />
+      <table>
+        <thead>
+          <tr>
+            <th>Tasks</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            tasksItem.map(task => (
+              <tr key={task.name}>
+                <td>{task.name}</td>
+              </tr>
+            ))
+          }
+        </tbody>
+      </table>
+
     </div>
   );
 }
---
+++ src/components/TaskCreator.jsx
@@ -0,0 +1,28 @@
+import {useState} from "react";
+
+const TaskCreator = ({createNewTask}) => {
+
+  const [newTaskName, setNewTaskName] = useState("");
+
+  const handleSubmit = (e) => {
+    
+    e.preventDefault(); // Nos cancela el por defecto que tienen los formularios de refrecar la pagina al darles enter.
+    createNewTask(newTaskName);
+    localStorage.setItem("tasks", newTaskName);
+    setNewTaskName("");// Con esto vacio al darle enter o click, el input se limpia.
+  }
+
+  return(
+    <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Ingrese su tarea"
+          value={newTaskName}
+          onChange={(e) => setNewTaskName(e.target.value)} // Guardamos lo escriot en el input.
+        />
+        <button>Agregar Tarea</button>
+      </form>
+  )
+}
+
+export default TaskCreator;
